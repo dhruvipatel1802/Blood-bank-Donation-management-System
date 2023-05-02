e737330deaceb0ce8f53a19b30446b9dbe41f9b2
Dhruvi Patel <dhruvipatel6355@gmail.com>
2023-05-03 00:08:03 +0530

    Blood-bank-Management-System

---
+++ footer.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+  <style>
+  #footer {
+
+  position:absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 92.5px;
+  background-color:#000000;
+  color:white;
+  text-align: center;
+  font-family: 'Comicsans';
+}
+  </style>
+</head>
+<body>
+  <div id="footer" >
+  <b><center>DEVELOPED BY<br>
+  21DCE079 DHRUVI PATEL<br>
+  21DCE091 PRIYANSHI PATEL<br>
+  21DCE097 TRUPAL PATEL<br>
+<br>
+
+  </center>
+  </div>
+
+
+</body>
+
+</html>
